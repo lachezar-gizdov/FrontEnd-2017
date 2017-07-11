7897012648d6e9cb9b2f06ca699472623a78543f
Lucho Gizdov <olebg01@gmail.com>
2017-07-11 11:40:49 +0300

    fix npm start command

---
+++ 07-Node-JS-Applications/00-Workshops/11.07-2017/Models/course.js
@@ -0,0 +1,24 @@
+class Course{
+    constructor(name, startingDate, endingDate, lecturesPerWeek){
+        this.name = name;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.lecturesPerWeek = lecturesPerWeek;
+    }
+
+    get Name(){
+        return this.name;
+    }
+
+    get StartingDate(){
+        return this.startingDate;
+    }
+
+    get EndingDate(){
+        return this.endingDate;
+    }
+
+    get LecturesPerWeek(){
+        return this.lecturesPerWeek;
+    }
+}
