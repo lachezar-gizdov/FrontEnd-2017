1c517a77adc06bb1ece2e93b31ade125227828eb
Lucho Gizdov <olebg01@gmail.com>
2017-07-16 16:11:44 +0300

    post pug extends layout

--- 07-Node-JS-Applications/00-Workshops/11.07-2017/routes/index.js
+++ 07-Node-JS-Applications/00-Workshops/11.07-2017/routes/index.js
@@ -9,4 +9,8 @@ router.get('/courses', function(req, res) {
   res.render('courses', { title: 'Express' });
 });
 
+router.get('/posts', function(req, res) {
+  res.render('posts', { title: 'Express' });
+});
+
 module.exports = router;
