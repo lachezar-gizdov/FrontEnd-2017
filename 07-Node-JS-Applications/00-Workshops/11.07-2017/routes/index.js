d3cdbdca290ebd1a668f34a7d2c4817d2b174996
Lucho Gizdov <olebg01@gmail.com>
2017-07-16 15:17:32 +0300

    extend the pug page for courses

--- 07-Node-JS-Applications/00-Workshops/11.07-2017/routes/index.js
+++ 07-Node-JS-Applications/00-Workshops/11.07-2017/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res) {
   res.render('home', { title: 'Express' });
 });
 
+router.get('/courses', function(req, res) {
+  res.render('courses', { title: 'Express' });
+});
+
 module.exports = router;
