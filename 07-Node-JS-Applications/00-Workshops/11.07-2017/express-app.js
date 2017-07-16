b3895672a30bb803bf5ebc92478c59421ba823dc
Lucho Gizdov <olebg01@gmail.com>
2017-07-16 14:41:45 +0300

    add working express server

--- 07-Node-JS-Applications/00-Workshops/11.07-2017/express-app.js
+++ 07-Node-JS-Applications/00-Workshops/11.07-2017/express-app.js
@@ -2,13 +2,9 @@
 
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
-const users = require('./routes/user');
 
 const app = express();
 
@@ -17,17 +13,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 app.use(function(req, res, next) {
     const err = new Error('Not Found');
@@ -43,7 +36,5 @@ app.use(function(err, req, res, next) {
         title: 'error',
     });
 });
-
-//test slack integration
 
 module.exports = app;
